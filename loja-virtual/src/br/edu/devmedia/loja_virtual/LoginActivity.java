f8da2dbf039aec4dc232249bd241289c5510577e
Tarcisio Costa Corte de Paul <tarcisiocorte@gmail.com>
2014-10-20 17:16:45 -0200

    teste sério

--- loja-virtual/src/br/edu/devmedia/loja_virtual/LoginActivity.java
+++ loja-virtual/src/br/edu/devmedia/loja_virtual/LoginActivity.java
@@ -10,38 +10,36 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-
-
-
 // outro comentário
 // fazer alguns comentarios pertinentes
 public class LoginActivity extends Activity {
 
 	private EditText edtLogin;
-	
+
 	private EditText edtSenha;
-	
+	private EditText edtTeste;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
+
 		edtLogin = (EditText) findViewById(R.id.edt_login);
 		edtSenha = (EditText) findViewById(R.id.edt_senha);
 	}
-	
+
 	public void logar(View view) {
 		new LoadingAsync().execute();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-//		menu.add(0, 1, 1, "Sair");
-//		menu.add(0, 2, 2, "Info");
+		// menu.add(0, 1, 1, "Sair");
+		// menu.add(0, 2, 2, "Info");
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		int idMenuItem = item.getItemId();
@@ -50,38 +48,40 @@ public class LoginActivity extends Activity {
 			finish();
 			break;
 		}
-		
+
 		return true;
 	}
-	
+
 	private class LoadingAsync extends AsyncTask<Void, Void, Void> {
 
-		private ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
-		
+		private ProgressDialog progressDialog = new ProgressDialog(
+				LoginActivity.this);
+
 		@Override
 		protected void onPreExecute() {
 			progressDialog.setMessage("Carregando...");
 			progressDialog.show();
 		}
-		
+
 		@Override
 		protected Void doInBackground(Void... params) {
 			String login = edtLogin.getText().toString();
 			String senha = edtSenha.getText().toString();
-			
+
 			boolean dadosValidos = true;
 			if (login == null || login.equals("")) {
 				dadosValidos = false;
 				edtLogin.setError(getString(R.string.msg_login_obg));
 			}
-			
+
 			if (senha == null || senha.equals("")) {
 				dadosValidos = false;
 				edtSenha.setError(getString(R.string.msg_senha_obg));
-			} 
-			
+			}
+
 			if (dadosValidos) {
-				Intent i = new Intent(LoginActivity.this, DashBoardActivity.class);
+				Intent i = new Intent(LoginActivity.this,
+						DashBoardActivity.class);
 				startActivity(i);
 				finish();
 			}
