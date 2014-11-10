c8e964f1662d626580445fd3a27a84d822a44736
Tarcisio Costa Corte de Paul <tarcisiocorte@gmail.com>
2014-11-10 08:21:01 -0200

    Adicionando listview

--- loja-virtual/src/br/edu/devmedia/loja_virtual/DashBoardActivity.java
+++ loja-virtual/src/br/edu/devmedia/loja_virtual/DashBoardActivity.java
@@ -1,7 +1,10 @@
 package br.edu.devmedia.loja_virtual;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DashBoardActivity extends Activity {
 
@@ -9,10 +12,25 @@ public class DashBoardActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);
-		
-		
-		
-		
 	}
-	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, 1, 1, "Lista");
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		int id = item.getItemId();
+		switch (id) {
+		case 1:
+			Intent intent = new Intent(this, DashboardListActivity.class);
+			startActivity(intent);
+			finish();
+			break;
+		}
+		return true;
+	}
+
 }
