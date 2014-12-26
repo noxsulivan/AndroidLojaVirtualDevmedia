fad689d780c186c993770f3733e3d27d1f0623c3
tarcisiocorte <tarcisiocorte@gmail.com>
2014-12-26 14:01:35 -0200

    ajustes na classe LoginBO

--- loja-virtual/src/br/edu/devmedia/loja_virtual/LoginActivity.java
+++ loja-virtual/src/br/edu/devmedia/loja_virtual/LoginActivity.java
@@ -17,41 +17,40 @@ import br.edu.devmedia.loja_virtual.util.MensagemUtil;
 
 public class LoginActivity extends Activity {
 
-	LoginBO loginBo;
-
+	private LoginBO loginBO;
+	
 	private EditText edtLogin;
-
+	
 	private EditText edtSenha;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-
+		
 		edtLogin = (EditText) findViewById(R.id.edt_login);
 		edtSenha = (EditText) findViewById(R.id.edt_senha);
 	}
-
+	
 	public void logar(View view) {
 		new LoadingAsync().execute();
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// menu.add(0, 1, 1, "Sair");
-		// menu.add(0, 2, 2, "Info");
+//		menu.add(0, 1, 1, "Sair");
+//		menu.add(0, 2, 2, "Info");
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-
+	
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		int idMenuItem = item.getItemId();
 		switch (idMenuItem) {
 		case R.id.menuSair:
-			MensagemUtil.addMsgConfirm(LoginActivity.this, "Logout",
-					"Deseja deslogar?", R.drawable.logout,
-					new OnClickListener() {
+			MensagemUtil.addMsgConfirm(LoginActivity.this, getString(R.string.lbl_sair), 
+					getString(R.string.msg_logout), R.drawable.logout, new OnClickListener() {
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
 							finish();
@@ -59,48 +58,46 @@ public class LoginActivity extends Activity {
 					});
 			break;
 		case R.id.menuSobre:
-			MensagemUtil.addMsgOk(LoginActivity.this,
-					getString(R.string.lbl_sobre),
-					getString(R.string.msg_sobre), R.drawable.i);
+			MensagemUtil.addMsgOk(LoginActivity.this, getString(R.string.lbl_sobre), 
+					getString(R.string.msg_sobre), R.drawable.about);
 			break;
 		}
-
+		
 		return true;
 	}
-
+	
 	private class LoadingAsync extends AsyncTask<Void, Void, String> {
 
-		private ProgressDialog progressDialog = new ProgressDialog(
-				LoginActivity.this);
-
+		private ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+		
 		@Override
 		protected void onPreExecute() {
 			progressDialog.setMessage("Carregando...");
 			progressDialog.show();
 		}
-
+		
 		@Override
 		protected String doInBackground(Void... params) {
 			String login = edtLogin.getText().toString();
 			String senha = edtSenha.getText().toString();
-
-			loginBo = new LoginBO(LoginActivity.this);
-
-			return loginBo.validarLogin(login, senha);
+			
+			loginBO = new LoginBO(LoginActivity.this);
+			
+			return loginBO.validarLogin(login, senha);
 		}
 
 		@Override
-		protected void onPostExecute(String result) {
+		protected void onPostExecute(String msg) {
 			progressDialog.dismiss();
-
-			if (result == null) {
-				Intent i = new Intent(LoginActivity.this,
-						DashBoardActivity.class);
+			
+			if (msg == null) {
+				Intent i = new Intent(LoginActivity.this, DashBoardActivity.class);
 				startActivity(i);
 				finish();
 			} else {
-				MensagemUtil.addMsg(LoginActivity.this, result);
+				MensagemUtil.addMsg(LoginActivity.this, msg);
 			}
 		}
 	}
+
 }
